e3e28cbba1c91345654164398e8b781d99f8e9f6
NaradaJ <140716249+NaradaJ@users.noreply.github.com>
2023-11-28 21:56:51 +0530

    28.11.2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ReactMarkdown from "react-markdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()  {
+  const[markdownText,setMarkdownText] = useState(0);
+
+  return<>
+  <div>
+    <h1>Markdown Previewer</h1>
+  </div>
+  </>
 }
+
 
 export default App;
